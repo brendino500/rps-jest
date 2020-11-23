cf93d31c917feb48dc062745aefdc4bbb4885efb
brendino500 <brenda.ty@live.com>
2020-11-23 16:44:44 +0000

    initial state setup App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
+import React from "react";
+
 function App() {
+  const [playerScore, setPlayerScore] = React.useState(0);
+  const [computerScore, setComputerScore] = React.useState(0);
+  const [options, setOption] = React.useState(["rock", " paper", "scissors"]);
+  const [playerChoice, setPlayerChoice] = React.useState("");
+  const [computerChoice, setComputerChoice] = React.useState("");
+  const [winnerResults, setWinnerResults] = React.useState("");
+
   return <div className="container" data-test="component-app"></div>;
 }
 
