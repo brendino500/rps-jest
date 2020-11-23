e1e6a3be328cf6db8bf07f1095aabf6337a522fc
brendino500 <brenda.ty@live.com>
2020-11-23 15:17:46 +0000

    :star2: initial setup :star2:

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { mount } from "enzyme";
+import { render, screen } from "@testing-library/react";
+import { findByTestAttr } from "./test/testUtils";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+const setup = () => {
+  return mount(<App />);
+};
+
+test("App renders without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-app");
+  expect(component.length).toBe(1);
 });
