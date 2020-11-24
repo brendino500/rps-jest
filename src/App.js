02c68ab86a6329031cc05703462dd6e069da6271
brendino500 <brenda.ty@live.com>
2020-11-24 10:31:38 +0000

    test buttons setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React from "react";
+import ChoiceButtons from "./ChoiceButtons";
 
 function App() {
   const [playerScore, setPlayerScore] = React.useState(0);
   const [computerScore, setComputerScore] = React.useState(0);
-  const [options, setOption] = React.useState(["rock", " paper", "scissors"]);
+  const [options, setOptions] = React.useState(["rock", " paper", "scissors"]);
   const [playerChoice, setPlayerChoice] = React.useState("");
   const [computerChoice, setComputerChoice] = React.useState("");
   const [winnerResults, setWinnerResults] = React.useState("");
 
-  return <div className="container" data-test="component-app"></div>;
+  const handleClick = (e) => {
+    setComputerChoice(compChoice());
+    setPlayerChoice(e.target.value);
+    setWinnerResults(compareChoices(computerChoice, playerChoice));
+  };
+
+  const handleReset = () => {
+    setComputerChoice("");
+    setPlayerChoice("");
+    setWinnerResults("");
+    setOptions(["rock", " paper", "scissors"]);
+  };
+
+  const compChoice = () => {
+    return options[Math.floor(Math.random() * options.length)];
+  };
+
+  const compareChoices = (computerChoice, playerChoice) => {
+    if (playerChoice === computerChoice) return "Draw";
+    if (
+      computerChoice === options[0] ||
+      computerChoice === options[1] ||
+      computerChoice === options[2]
+    ) {
+      return "You Win";
+    }
+    return "You Lost";
+  };
+
+  return (
+    <div className="container" data-test="component-app">
+      {/* <Main />
+      <Results
+        playerChoice={playerChoice}
+        computerChoice={computerChoice}
+        playerScore={playerScore}
+        computerScore={computerScore}
+        winnerResults={winnerResults}
+      /> */}
+      <ChoiceButtons handleClick={handleClick} handleReset={handleReset} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Main.test.js
@@ -0,0 +1,15 @@
+import { mount } from "enzyme";
+import { render, screen } from "@testing-library/react";
+import { findByTestAttr } from "./test/testUtils";
+
+import Main from "./Main";
+
+const setup = () => {
+  return mount(<Main />);
+};
+
+test("App renders without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-main");
+  expect(component.length).toBe(1);
+});
