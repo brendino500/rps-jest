f7c8fbf58222ddc14488304fb9d39dccecfa9378
brendino500 <brenda.ty@live.com>
2021-01-08 09:49:56 +0000

    fully responsive

--- src/ChoiceButtons.js
+++ src/ChoiceButtons.js
@@ -10,7 +10,10 @@ export default function Choices({ handleClick, handleReset }) {
   const classes = useStyles();
   return (
     <>
-      <section className="buttons" data-test="component-choice-buttons">
+      <section
+        className={("buttons", classes.buttons)}
+        data-test="component-choice-buttons"
+      >
         <Grid
           container
           direction="row"
@@ -66,16 +69,32 @@ const useStyles = makeStyles((theme) => ({
   images: {
     height: 200,
     width: "8em",
-    value: "scissors",
+    [theme.breakpoints.between("xs", "sm")]: {
+      width: 100,
+    },
+    [theme.breakpoints.between("sm", "md")]: {
+      width: 200,
+    },
+  },
+  buttons: {
+    [theme.breakpoints.between("sm", "md")]: {
+      marginTop: "2em",
+    },
   },
   resetText: {
     fontFamily: "Roboto Slab",
     letterSpacing: 4,
     fontWeight: "bold",
     fontSize: 25,
+    [theme.breakpoints.between("xs", "sm")]: {
+      fontSize: 18,
+    },
   },
   resetButton: {
     display: "flex",
     justifyContent: "center",
+    [theme.breakpoints.between("sm", "md")]: {
+      marginTop: "2em",
+    },
   },
 }));
--- src/Results.js
+++ src/Results.js
@@ -71,6 +71,13 @@ const useStyles = makeStyles((theme) => ({
   scoreText: {
     fontFamily: "Roboto Slab",
     fontSize: 30,
+    [theme.breakpoints.between("xs", "sm")]: {
+      fontSize: 18,
+      marginTop: "2em",
+    },
+    [theme.breakpoints.between("sm", "md")]: {
+      fontSize: 34,
+    },
   },
   scoreGrid: {
     display: "flex",
@@ -79,9 +86,18 @@ const useStyles = makeStyles((theme) => ({
   },
   results: {
     marginTop: "-5em",
+    [theme.breakpoints.between("sm", "md")]: {
+      marginTop: "-2em",
+    },
   },
   roundResults: {
     textAlign: "center",
+    [theme.breakpoints.between("xs", "sm")]: {
+      marginTop: "2em",
+    },
+    [theme.breakpoints.between("sm", "md")]: {
+      marginTop: "2em",
+    },
   },
   winnerResults: {
     textAlign: "center",
@@ -90,5 +106,11 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 20,
     fontFamily: "Roboto Slab",
     margin: "0.55em 0",
+    [theme.breakpoints.between("xs", "sm")]: {
+      fontSize: 18,
+    },
+    [theme.breakpoints.between("sm", "md")]: {
+      fontSize: 28,
+    },
   },
 }));
